a16028403d1076ee918e7083decc461f7a4490cb
Alojzij Blatnik <lojze.blatnik@gmail.com>
2016-02-07 17:12:23 +0100

    resolution changes

--- jni/farneb.cpp
+++ jni/farneb.cpp
@@ -56,8 +56,8 @@ void diffclock(char const *title, clock_t clock2) {
 
     doLogClock("%s: %f", title, diffms);
 }
-int leftXOffset=100, leftYOffset=170, leftCols=100, leftRows=100;
-int rightXOffset=250, rightYOffset=170, rightCols=100, rightRows=100;
+int leftXOffset=100, leftYOffset=130, leftCols=100, leftRows=100;
+int rightXOffset=210, rightYOffset=130, rightCols=100, rightRows=100;
 
 int leftXp1=0, leftYp1=0, rightXp1=0, rightYp1=0;
 int leftXlast=0, leftYlast=0, rightXlast=0, rightYlast=0;
@@ -191,11 +191,16 @@ int process(Mat frame, Mat gray, Mat out) {
     Mat left, right;
     Mat flowLeft, flowRight;
 
-    if (faceDetect(gray, &face, &faceROI) != 0) { return -1; }
+    if (faceDetect(gray, &face, &faceROI) != 0) {
+        if (DEBUG_LEVEL >= 1 && PHONE == 0) {
+            imshow("main", out);
+        }
+
+        return -1;
+    }
 
     eyeRegions(face, &leftEyeRegion, &rightEyeRegion);
     eyeCenters(faceROI, leftEyeRegion, rightEyeRegion, &leftPupil, &rightPupil);
-
     getLeftRightEyeMat(gray, leftEyeRegion, rightEyeRegion,  &left, &right);
 
     if (firstLoopProcs == 0) {
@@ -210,8 +215,7 @@ int process(Mat frame, Mat gray, Mat out) {
         start = clock();
         drawOptFlowMap(flowLeft, out, 10, Scalar(0, 255, 0), 0);
         drawOptFlowMap(flowRight, out, 10, Scalar(0, 255, 0), 1);
-//imshow("fris", faceROI);
-        
+
         //drawOptFlowMap(flowLeft, *out, 10, CV_RGB(0, 255, 0));
         diffclock("- drawOptFlowMap", start);
         if (DEBUG_LEVEL >= 2 && PHONE == 0) {
@@ -230,15 +234,23 @@ int process(Mat frame, Mat gray, Mat out) {
 int main() {
     PHONE = 0; farne = 0;
     // test videos
-    // char fileName[100] = "/home/developer/other/posnetki/o4_29.mp4";
-    // char fileName1[200] = "/home/developer/other/test_videos/crnc1.mp4";
-    // char fileName1[200] = "/home/developer/other/test_videos/indian_close.mp4";
-    // char fileName1[200] = "/home/developer/other/test_videos/yellow_close.mp4";
-    // char fileName1[200] = "/home/developer/other/test_videos/very_dark.mp4";
-    //char fileName[100] = "/opt/docker_volumes/mag/home_developer/other/posnetki/o4_29.mp4";
-    // VideoCapture stream1(fileName1);   //0 is the id of video device.0 if you have only one camera   
-    VideoCapture stream1(0);   //0 is the id of video device.0 if you have only one camera   
+    char fileName[100] = "/home/developer/other/posnetki/o4_29.mp4";
+    // char fileName[200] = "/home/developer/other/test_videos/crnc1.mp4";
+    // char fileName[200] = "/home/developer/other/test_videos/indian_close.mp4";
+    // char fileName[200] = "/home/developer/other/test_videos/yellow_close.mp4";
+    // char fileName[200] = "/home/developer/other/test_videos/very_dark.mp4";
+    // char fileName[100] = "/opt/docker_volumes/mag/home_developer/other/posnetki/o4_29.mp4";
+    // VideoCapture stream1(fileName);   //0 is the id of video device.0 if you have only one camera
+    VideoCapture stream1(0);
+    if (!stream1.isOpened()) {
+        CV_Assert("Cam open failed");
+    }
+
+    // resolutions 320, 240; 800, 448; 640, 480
+    stream1.set(CV_CAP_PROP_FRAME_WIDTH, 640); stream1.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+
     char faceDetector[200] = "/home/developer/other/android_deps/OpenCV-2.4.10-android-sdk/samples/optical-flow/res/raw/lbpcascade_frontalface.xml";
+    // printf("video capture %f %f %f\n", stream1.get(CV_CAP_PROP_FRAME_WIDTH), stream1.get(CV_CAP_PROP_FRAME_HEIGHT), stream1.get(CV_CAP_PROP_FPS));
 
     // controls
     int pause = 0, firstLoop = 1;
@@ -249,17 +261,18 @@ int main() {
 
     if (farne == 0) {
         cv::namedWindow(face_window_name,CV_WINDOW_NORMAL); cv::moveWindow(face_window_name, 10, 100);
-        cv::namedWindow("main",CV_WINDOW_NORMAL); cv::moveWindow("main", 10, 100);
+        cv::namedWindow("main",CV_WINDOW_NORMAL); cv::moveWindow("main", 10, 100);// resizeWindow("main",1280, 960);
         // cv::namedWindow("Right Eye",CV_WINDOW_NORMAL); cv::moveWindow("Right Eye", 10, 600);
         // cv::namedWindow("Left Eye",CV_WINDOW_NORMAL); cv::moveWindow("Left Eye", 10, 800);
         // createCornerKernels(), at the end // releaseCornerKernels(); // ellipse(skinCrCbHist, cv::Point(113, 155.6), cv::Size(23.4, 15.2), 43.0, 0.0, 360.0, cv::Scalar(255, 255, 255), -1);
     }
 
-    while (true) { cv::flip(frame, frame, 1);
+    while (true) {
         if(!(stream1.read(frame))) {
             doLog(" --(!) No captured frame -- Break!");
             return 0;
         }
+        // cv::flip(frame, frame, 1);
 
         if (firstLoop == 1) {
             loopStart = clock();
