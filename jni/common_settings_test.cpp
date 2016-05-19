d0501b3474583cfc7b4fd963dc65bc2beb238531
Alojzij Blatnik <lojze.blatnik@gmail.com>
2016-05-19 12:40:15 +0200

    upd refactoring settings and compile

---
+++ jni/common_settings_test.cpp
@@ -0,0 +1,28 @@
+#include <common.hpp>
+
+// TODO this could be done using macros (at all places is used ifdef)
+bool debug_show_img_main = true;
+bool debug_show_img_face = false;
+bool debug_show_img_optfl_eyes = false;
+bool debug_show_img_templ_eyes_cor = false;
+bool debug_show_img_templ_eyes_tmpl = false;
+bool debug_t1_log = false;
+bool debug_t2_log = false;
+bool debug_t2_perf_method = false;
+bool debug_t2_perf_whole = false;
+bool debug_tmpl_log = true;
+bool debug_tmpl_perf1 = false;
+bool debug_tmpl_perf2 = false;
+bool debug_notifications_log1 = true;
+bool debug_blinks_d1 = true;
+bool debug_blinks_d2 = true;
+bool debug_blinks_d3 = true;
+bool debug_blinks_d4 = true;
+
+//int method = METHOD_OPTFLOW;
+int method = METHOD_TEMPLATE_BASED;
+double previousFrameTime = -1;
+
+// optical flow
+int flg=0;
+///
