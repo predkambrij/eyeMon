939133457792c7eda2560eee6062fbced3150fda
Alojzij Blatnik <lojze.blatnik@gmail.com>
2016-07-28 11:29:19 +0200

    fix cascade path

--- jni/common_settings_comp.cpp
+++ jni/common_settings_comp.cpp
@@ -2,7 +2,7 @@
 
 // TODO this could be done using macros (at all places is used ifdef)
 bool debug_show_img_d1 = false;
-bool debug_show_img_main = false;
+bool debug_show_img_main = true;
 bool debug_show_img_gray = false;
 bool debug_show_img_face = false;
 bool debug_show_img_templ_eyes_cor = true;
@@ -45,8 +45,8 @@ bool shouldUseAnnotTimestamps = false;
 
 
 //int method = METHOD_OPTFLOW;
-int method = METHOD_TEMPLATE_BASED;
-//int method = METHOD_FARNEBACK;
+//int method = METHOD_TEMPLATE_BASED;
+int method = METHOD_FARNEBACK;
 //int method = METHOD_BLACKPIXELS;
 double previousFrameTime = -1;
 
--- jni/main.cpp
+++ jni/main.cpp
@@ -393,7 +393,8 @@ int main() { // int argc, char * argv[]
 #endif
     PHONE = 0;
 
-    char faceDetector[200] = "/home/developer/other/android_deps/OpenCV-2.4.10-android-sdk/samples/optical-flow/res/raw/lbpcascade_frontalface.xml";
+    //char faceDetector[200] = "/home/developer/other/android_deps/OpenCV-2.4.10-android-sdk/samples/optical-flow/res/raw/lbpcascade_frontalface.xml";
+    char faceDetector[200] = "res/raw/lbpcascade_frontalface.xml";
     switch (method) {
         case METHOD_OPTFLOW:
         optf.setup(faceDetector);
