91c3dd958052c9366210f3168e27d0bf39144390
Alojzij Blatnik <lojze.blatnik@gmail.com>
2016-08-07 19:55:05 +0200

    upd project transformation into android studio

---
+++ _1OpenCVopticalflow/src/main/jni/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+#OPENCV_CAMERA_MODULES:=off
+#OPENCV_INSTALL_MODULES:=off
+#OPENCV_LIB_TYPE:=SHARED
+#include ../../sdk/native/jni/OpenCV.mk
+include /home/developer/OpenCV-2.4.10-android-sdk/sdk/native/jni/OpenCV.mk
+#include ../../../OpenCV-2.4.10-android-sdk/sdk/native/jni/OpenCV.mk
+#include ../../../OpenCV-android-sdk-2-4-11/sdk/native/jni/OpenCV.mk
+
+LOCAL_SRC_FILES  := common_settings_phone.cpp common.cpp blinkmeasure.cpp blinkmeasuref.cpp farneback_jni.cpp farneback.cpp optflow_jni.cpp optflow.cpp templatebased.cpp templatebased_jni.cpp eyeLike/src/findEyeCenter.cpp eyeLike/src/helpers.cpp
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_LDLIBS     += -llog -ldl
+
+LOCAL_CFLAGS += -std=c++11 -DIS_PHONE
+
+LOCAL_MODULE     := eyemon
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ _1OpenCVopticalflow/src/main/jni/eyeLike/src/findEyeCenter.h
@@ -0,0 +1,9 @@
+#ifndef EYE_CENTER_H
+#define EYE_CENTER_H
+
+#include "opencv2/imgproc/imgproc.hpp"
+
+cv::Point findEyeCenter(cv::Mat face, cv::Rect eye);
+cv::Mat floodKillEdges(cv::Mat &mat);
+
+#endif
